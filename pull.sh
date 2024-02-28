d24302356f708194380074ba6202aa0726b6c0c9
rushig71 <gunjekar.rushi@gmail.com>
2024-02-28 14:49:36 +0530

    added script file pull.sh

---
+++ pull.sh
@@ -0,0 +1,16 @@
+if git remote | grep origin > /dev/null; then
+    echo "origin found"
+else
+    git remote add origin https://github.com/rushig71/fictional-carnival.git
+    git checkout main
+fi
+
+if git log | grep git-subtree-dir | tr -d ' ' | cut -d ":" -f2 | sort | uniq | grep sub-folder > /dev/null; then
+    echo "subtree found"
+else
+    git subtree add --prefix=sub-folder https://github.com/rushig71/stunning-rotary-phone.git main --squash
+fi
+
+git pull
+git subtree pull --prefix=sub-folder https://github.com/rushig71/stunning-rotary-phone.git main --squash
+git push
