de3a8dfa0b12689ca7caba8abb7153eafb4a42d4
rushig71 <gunjekar.rushi@gmail.com>
2024-02-28 16:08:28 +0530

    edited script file pull.sh

--- pull.sh
+++ pull.sh
@@ -1,7 +1,7 @@
 if git remote | grep origin > /dev/null; then
-    git remote set-url origin https://rushig71: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@github.com/rushig71/fictional-carnival.git
+    git remote set-url origin https://rushig71:dx11rDCVa!@github.com/rushig71/fictional-carnival.git
 else
-    git remote add origin https://rushig71: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@github.com/rushig71/fictional-carnival.git
+    git remote add origin https://rushig71:dx11rDCVa!@github.com/rushig71/fictional-carnival.git
 fi
 
 git pull origin main
